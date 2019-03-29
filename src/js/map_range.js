de256fb407686190615b96660cab5af6afd289ec
= <=>
2019-03-29 18:41:53 +0100

    remove all sources, only keep build

--- src/js/map_range.js
+++
@@ -1,6 +0,0 @@
-function map_range(value, min_input, max_input, min_output, max_output) {
-    value = (value - min_input) / (max_input - min_input);
-    return min_output + value * (max_output - min_output);
-}
-
-export default map_range;
